29d9cc81e116e3175f1235bd6acb8c6602019342
Paul Velasquez <velpol.eirl@gmail.com>
2026-02-14 02:29:10 -0500

    fix tabs a espacios

--- main.py
+++ main.py
@@ -188,16 +188,16 @@ def procesar_vin(vin: str):
 
     return pais, fabricante, anio
 def validar_vin_matematico(vin: str):
- 	valores = {str(i): i for i in range(10)}
+    valores = {str(i): i for i in range(10)}
 
-	valores.update({
-   	 "A": 1, "B": 2, "C": 3, "D": 4, "E": 5,
-   	 "F": 6, "G": 7, "H": 8, "J": 1,
- 	 "K": 2, "L": 3, "M": 4, "N": 5,
-   	 "P": 7, "R": 9, "S": 2, "T": 3,
-  	 "U": 4, "V": 5, "W": 6, "X": 7,
-   	 "Y": 8, "Z": 9
-})
+    valores.update({
+        "A": 1, "B": 2, "C": 3, "D": 4, "E": 5,
+        "F": 6, "G": 7, "H": 8, "J": 1,
+        "K": 2, "L": 3, "M": 4, "N": 5,
+        "P": 7, "R": 9, "S": 2, "T": 3,
+        "U": 4, "V": 5, "W": 6, "X": 7,
+        "Y": 8, "Z": 9
+    })
 
     pesos = [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2]
 
